489317c7b3fde22522f4d60bd2b8319e614a4ff2
josh cheng <promethus@gmail.com>
2020-01-07 15:52:15 +0800

    editText監聽事件完成

--- app/src/main/java/com/example/myktfirebasestore/Main2Activity.kt
+++ app/src/main/java/com/example/myktfirebasestore/Main2Activity.kt
@@ -11,8 +11,7 @@ import android.widget.ListView
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.toObject
-import com.google.protobuf.LazyStringArrayList
+import com.google.protobuf.compiler.PluginProtos
 import kotlinx.android.parcel.Parcelize
 import kotlinx.android.synthetic.main.activity_main2.*
 import java.lang.Exception
@@ -20,7 +19,6 @@ import java.lang.ref.WeakReference
 import java.util.*
 import kotlin.Comparator
 import kotlin.collections.ArrayList
-import kotlin.collections.HashMap
 
 
 data class User2(
@@ -31,6 +29,7 @@ data class User2(
 
 @Parcelize
 class CitiesDataType() : ArrayList<String>(), Parcelable
+
 
 class Main2Activity : AppCompatActivity() {
 
@@ -45,39 +44,92 @@ class Main2Activity : AppCompatActivity() {
             }
 
             override fun handleMessage(msg: Message) {
+                super.handleMessage(msg)
                 when (msg.what) {
                     GetCities.TagNum -> {
                         Log.d(GetCities.Tag, "TestTask.arg1=${msg.arg1}")
 
-                        val list = msg.data.getParcelable<CitiesDataType>(GetCities.Tag)
-
                         val act = mOuter?.get()
-                        val lv = act?.findViewById<ListView>(R.id.lvCities)
-                        if (act == null || list == null) return
+                        val list = msg.data.getParcelable<CitiesDataType>(GetCities.Tag)
+                        if (list != null && act != null) {
 
-                        lv?.adapter = ArrayAdapter<String>(
-                            act,
-                            android.R.layout.simple_list_item_1,
-                            list
-                        )
+                            list.sort()
+                            act.cities = list
+                            val listSearched = act.citiesSearing
+                            for (it in list) listSearched.add(it.toLowerCase())
+
+                            for (i in 0..30) println("$i) ${listSearched[i]}")
+                        }
+//
+//                        val lv = act?.findViewById<ListView>(R.id.lvCities)
+//                        if (act == null || list == null) return
+//
+//                        lv?.adapter = ArrayAdapter<String>(
+//                            act,
+//                            android.R.layout.simple_list_item_1,
+//                            list
+//                        )
 
                     }
+                    SearchingTask.TagNum -> {
+                        val act = mOuter?.get()
+                        val lv = act?.findViewById<ListView>(R.id.lvCities)
+                        val list = msg.data.getParcelable<CitiesDataType>(GetCities.Tag)
+                        if (act == null || list == null) return
+                        lv?.adapter =
+                            ArrayAdapter<String>(act, android.R.layout.simple_list_item_1, list)
+                        lv?.visibility = View.VISIBLE
+                    }
                 }
-                super.handleMessage(msg)
+
             }
         }
 
-//        class GetSearingList(val mHdle: Handler, val cities:CitiesDataType, val strSrc:String) : Runnable {
-//            override fun run() {
-//
-//
-//            }
-//
-//            companion object {
-//                val TagNum = 12
-//                val Tag = GetSearingList.javaClass.simpleName
-//            }
-//        }
+        class SearchingTask(
+            val mHdle: Handler,
+            val mSrcStrList: CitiesDataType,
+            val mStrSearchingList: CitiesDataType,
+            val targetStr: String
+        ) : Runnable {
+            override fun run() {
+                var tstr = targetStr.toLowerCase()
+
+
+                var pos = mStrSearchingList.binarySearch(tstr)
+                val result = CitiesDataType()
+                if (pos >= 0) {//match case
+                    result.add(mSrcStrList[pos])
+
+                } else {//insert case
+                    pos = Math.abs(pos + 1)
+                    for (i in pos..(mStrSearchingList.count() - 1)) {
+                        val strCity = mStrSearchingList[i]
+
+                        if (strCity.length < tstr.length) continue
+                        val substr = strCity.substring(0..(tstr.length - 1))
+
+                        if (tstr.compareTo(substr) == 0) {
+                            result.add(mSrcStrList[i])
+                        } else break
+                    }
+                }
+
+                val msg = mHdle.obtainMessage()
+                val bdl = Bundle().apply {
+                    putParcelable(Tag, result)
+//                    putStringArrayList(SearchingTask.Tag, result)
+                }
+                msg.what = TagNum
+                msg.data = bdl
+                mHdle.sendMessage(msg)
+            }
+
+            companion object {
+                val TagNum = 12
+                val Tag = SearchingTask.javaClass.simpleName
+            }
+        }
+
 
         class GetCities(val mHdle: Handler) : Runnable {
             override fun run() {
@@ -85,22 +137,14 @@ class Main2Activity : AppCompatActivity() {
                 val str = "cities_en"
                 db.collection("cities").document(str).get()
                     .addOnSuccessListener { doc ->
-
-                        if (doc != null) {
-
-//                            val ma = doc.data as HashMap<String, List<String?>>
-
                             val ma = doc.getData()
-
-                            if (ma == null) throw Exception("cities get error 01")
-                            val lst = ma[str]
-                            if (lst == null) throw Exception("cities get error 02")
-
+                        val lst = ma?.get(str)
                             if (lst is List<*> && lst.count() > 0) {
                                 val ary = CitiesDataType()
                                 for (it in lst) {
                                     if (it is String) ary.add(it)
                                 }
+
                                 val msg = mHdle.obtainMessage()
                                 Log.d(Tag, "TestTask.what=${msg.what}")
                                 msg.what = TagNum
@@ -110,12 +154,7 @@ class Main2Activity : AppCompatActivity() {
                                 }
                                 mHdle.sendMessage(msg)
                             }
-
-
-                        }
-
                     }
-
             }
 
             companion object {
@@ -127,27 +166,36 @@ class Main2Activity : AppCompatActivity() {
 
     val mHdle = MyHandler(this)
 
-
-    lateinit var mThread: Thread
+    lateinit var listView: ListView
+    //    lateinit var mThread: Thread
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        mThread = Thread(GetCities(MyHandler(this)))
-        mThread.start()
+        listView = findViewById<ListView>(R.id.lvCities)
+        Thread(GetCities(MyHandler(this))).start()
+
 
 
         edtInput.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
+                listView.visibility = View.INVISIBLE
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                if (cities.count() == 0 || s == null || s.length == 0) return
+                Thread(
+                    SearchingTask(
+                        MyHandler(this@Main2Activity),
+                        cities,
+                        citiesSearing,
+                        s?.toString()
+                    )
+                ).start()
             }
         })
     }
@@ -156,6 +204,8 @@ class Main2Activity : AppCompatActivity() {
     val email = "asx@gtc.com.tw"
     val collName = "users"
     val TAG = "err"
+    var cities = CitiesDataType()
+    var citiesSearing = CitiesDataType()
     fun onGetDataFromFF(v: View) {
 
         val db = FirebaseFirestore.getInstance()
